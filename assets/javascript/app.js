f0a946b697a0f32717f66191c730a3b5820eea25
Britney Ross <britneyross25@yahoo.com>
2020-05-29 21:27:39 -0500

    user can only select one answer per question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,46 +5,55 @@ $(document).ready(function () {
             question: "What is the hottest planet in the solar system?",
             choices: ["Mars", "Venus", "Saturn", "Jupiter"],
             answer: "Venus",
+            group: "1"
         },
         {
             question: "How many bones are in the human body?",
             choices: ["206", "205", "201", "209"],
             answer: "206",
+            group: "2"
         },
         {
             question: "Which ocean is the largest?",
             choices: ["Indian", "Pacific", "Atlantic", "Arctic"],
             answer: "Pacific",
+            group: "3"
         },
         {
             question: "What is the longest river in the world?",
             choices: ["Amazon", "Congo", "Nile", "Hudson"],
             answer: "Nile",
+            group: "4"
         },
         {
             question: "How many elements are there in the periodic table?",
             choices: ["112", "118", "120", "143"],
             answer: "118",
+            group: "5"
         },
         {
             question: "How many time zones are there in the world?",
             choices: ["7", "24", "23", "9"],
             answer: "24",
+            group: "6"
         },
         {
             question: "In 2016, a musician won the Nobel Peace Prize for Literature. Who was it?",
             choices: ["Eric Clapton", "Elton John", "Lenny Kravitz", "Bob Dylan"],
             answer: "Bob Dylan",
+            group: "7"
         },
         {
             question: "How many people have walked on the moon?",
             choices: ["10", "8", "12", "14"],
             answer: "12",
+            group: "8"
         },
         {
             question: "How many soccer players are on the field per team during a match?",
             choices: ["8", "12", "11", "10"],
             answer: "11",
+            group: "9"
         }
     ];
 
@@ -77,10 +86,12 @@ $(document).ready(function () {
             $("#question").append(`<h3> ${trivia[i].question} </h3>`)
             for (let j = 0; j < trivia[i].choices.length; j++) {
                 if (trivia[i].choices[j] === trivia[i].answer) {
-                    $("#question").append(`<button class="yes"> ${trivia[i].choices[j]} </button>`)
-
+             
+                    $("#question").append(`<input type="radio" class="yes" id=${trivia[i].choices[j]} name=${trivia[i].group} value=${trivia[i].choices[j]}>`)
+                    $("#question").append(`<label for=${trivia[i].choices[j]}>${trivia[i].choices[j]}</label><br></br>`)
                 } else {
-                    $("#question").append(`<button class="no"> ${trivia[i].choices[j]} </button>`)
+                    $("#question").append(`<input type="radio" class="no" id=${trivia[i].choices[j]} name=${trivia[i].group} value=${trivia[i].choices[j]}>`)
+                    $("#question").append(`<label for=${trivia[i].choices[j]}>${trivia[i].choices[j]}</label><br></br>`)
 
                 }
             }
